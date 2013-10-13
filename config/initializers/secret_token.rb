2fadaa7570ec17b87f0278e2430f6f8e420ab6a7
Graeme <graeme.barnes@cloudbi.co.za>
2013-10-13 19:14:31 +0200

    add devise gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MomApp::Application.config.secret_key_base = '027388d65fc9c9675caff48234e62d4e2344c7fec46e8e45efcad0ce9b5d8c6ee739ee072978a75132da94e726e1ae1e72df995223ff8a116efcbc6ff0ecbcd8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MomApp::Application.config.session_store :cookie_store, key: '_mom_app_session'
